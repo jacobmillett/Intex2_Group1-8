3ca601d3ec628159cacc954f2dcc302340a810c5
jacobmillett <jakemillett1@gmail.com>
2024-04-08 17:46:15 -0600

    Initial commit

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+using System.Diagnostics;
+using Microsoft.AspNetCore.Mvc;
+using AuroraBricks.Models;
+
+namespace AuroraBricks.Controllers;
+
+public class HomeController : Controller
+{
+    private readonly ILogger<HomeController> _logger;
+
+    public HomeController(ILogger<HomeController> logger)
+    {
+        _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+        return View();
+    }
+
+
+    public IActionResult ProductList()
+    {
+        return View();
+    }
+
+    public IActionResult ProductDetail()
+    {
+        return View();
+    }
+
+    public IActionResult AboutUs()
+    {
+        return View();
+    }
+
+    public IActionResult Cart()
+    {
+        return View();
+    }
+    
+    
+    
+    
+
+    public IActionResult Privacy()
+    {
+        return View();
+    }
+
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public IActionResult Error()
+    {
+        return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+    }
+}
