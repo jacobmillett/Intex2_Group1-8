4feee0167aaa0bc09a02b76bc6741cd22c49db5d
calebreese <calebreese@CALEBREESE63D5>
2024-04-10 15:09:26 -0600

    commit attempt

--- obj/Debug/net8.0/AuroraBricks.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuroraBricks.AssemblyInfo.cs
@@ -16,7 +16,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuroraBricks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61f8cfce3abacaa9772ba3255260f224b438cfea")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7e7b6b35e79618486251921b4db67eb5a42b319")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuroraBricks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuroraBricks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
