9133e9be90078afbb3869551c3381f32e6377ef2
jacobmillett <jakemillett1@gmail.com>
2024-04-11 11:42:36 -0600

    gfdfgd

--- Program.cs
+++ Program.cs
@@ -4,86 +4,92 @@ using AuroraBricks.Data;
 using AuroraBricks.Areas.Identity.Data;
 using AuroraBricks.Models;
 
-var builder = WebApplication.CreateBuilder(args);
-var services = builder.Services;
-var configuration = builder.Configuration;
-
-builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+internal class Program
 {
-    googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
-    googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
-});
-services.AddAuthorization(options =>
-{
-    options.AddPolicy("AdminPolicy", policy =>
+    public static async Task Main(string[] args)
     {
-        policy.RequireRole("Admin");
-    });
-});
-// Add services to the container.
-var identityConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
-                               throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
-builder.Services.AddDbContext<AuroraBricksIdentityDbContext>(options =>
-    options.UseSqlite(identityConnectionString));
+        var builder = WebApplication.CreateBuilder(args);
+        var services = builder.Services;
+        var configuration = builder.Configuration;
 
-var generalConnectionString = builder.Configuration.GetConnectionString("ABrixConnection") ??
-                              throw new InvalidOperationException("Connection string 'ABrixConnection' not found.");
-builder.Services.AddDbContext<AbrixContext>(options =>
-    options.UseSqlite(generalConnectionString));
+        builder.Services.AddAuthentication().AddGoogle(googleOptions =>
+        {
+            googleOptions.ClientId = configuration["Authentication:Google:ClientId"];
+            googleOptions.ClientSecret = configuration["Authentication:Google:ClientSecret"];
+        });
+        services.AddAuthorization(options =>
+        {
+            options.AddPolicy("AdminPolicy", policy =>
+            {
+                policy.RequireRole("Admin");
+            });
+        });
+        // Add services to the container.
+        var identityConnectionString = builder.Configuration.GetConnectionString("DefaultConnection") ??
+                                       throw new InvalidOperationException("Connection string 'DefaultConnection' not found.");
+        builder.Services.AddDbContext<AuroraBricksIdentityDbContext>(options =>
+            options.UseSqlite(identityConnectionString));
 
-builder.Services.AddScoped<IBrixRepository, EfBrixRepository>();
+        var generalConnectionString = builder.Configuration.GetConnectionString("ABrixConnection") ??
+                                      throw new InvalidOperationException("Connection string 'ABrixConnection' not found.");
+        builder.Services.AddDbContext<AbrixContext>(options =>
+            options.UseSqlite(generalConnectionString));
 
-builder.Services.AddDatabaseDeveloperPageExceptionFilter();
+        builder.Services.AddScoped<IBrixRepository, EfBrixRepository>();
 
-
-
-
-builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
-    .AddRoles<IdentityRole>()
-    .AddEntityFrameworkStores<AuroraBricksIdentityDbContext>();
-builder.Services.AddControllersWithViews();
+        builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 
 
-var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseMigrationsEndPoint();
-}
-else
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
 
-app.UseHttpsRedirection();
-app.UseStaticFiles();
+        builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+            .AddRoles<IdentityRole>()
+            .AddEntityFrameworkStores<AuroraBricksIdentityDbContext>();
+        builder.Services.AddControllersWithViews();
 
-app.UseRouting();
 
-app.UseAuthentication();
-app.UseAuthorization();
+        var app = builder.Build();
 
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
-app.MapRazorPages();
+        // Configure the HTTP request pipeline.
+        if (app.Environment.IsDevelopment())
+        {
+            app.UseMigrationsEndPoint();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Home/Error");
+            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+            app.UseHsts();
+        }
 
-using (var scope = app.Services.CreateScope())
-{
-    var roleManager =
-        scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+        app.UseHttpsRedirection();
+        app.UseStaticFiles();
 
-    var roles = new[] { "Admin", "Customer" };
+        app.UseRouting();
 
-    foreach (var role in roles)
-    {
-        if(!await roleManager.RoleExistsAsync(role))
-        await roleManager.CreateAsync(new IdentityRole(role));
+        app.UseAuthentication();
+        app.UseAuthorization();
+
+        app.MapControllerRoute(
+            name: "default",
+            pattern: "{controller=Home}/{action=Index}/{id?}");
+        app.MapRazorPages();
+
+        using (var scope = app.Services.CreateScope())
+        {
+            var roleManager =
+                scope.ServiceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+
+            var roles = new[] { "Admin", "Customer" };
+
+            foreach (var role in roles)
+            {
+                if (!await roleManager.RoleExistsAsync(role))
+                    await roleManager.CreateAsync(new IdentityRole(role));
+            }
+
+
+        }
+        app.Run();
     }
-
-
 }
-app.Run();
