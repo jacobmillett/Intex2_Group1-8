bcf615024297fac4ae2d6b8b0452ef2fa32dc582
calebreese <calebreese@CALEBREESE63D5>
2024-04-11 01:38:47 -0600

    2am

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -1,6 +1,8 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using AuroraBricks.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace AuroraBricks.Controllers;
 
@@ -8,10 +10,12 @@ public class HomeController : Controller
 {
 
     private readonly ILogger<HomeController> _logger;
+    private readonly AbrixContext _context;
 
-    public HomeController(ILogger<HomeController> logger)
+    public HomeController(ILogger<HomeController> logger, AbrixContext context)
     {
         _logger = logger;
+        _context = context;
     }
 
     public IActionResult Index()
@@ -23,12 +27,45 @@ public class HomeController : Controller
     {
         return View();
     }
-    //
-    //
-    // public IActionResult ProductList()
-    // {
-    //     return View();
-    // }
+
+    //public IActionResult ProductList(int pageNum)
+    //{
+    //    int pageSize= 5;
+    //    var query = _context.BrixProducts.AsQueryable()
+    //        .OrderBy(x => x.Name)
+    //        .Skip((pageNum - 1) * pageSize)
+    //        .Take(pageSize);
+    //}
+
+    public IActionResult ProductList(int pageNum, string category, string primaryColor)
+    {
+        int pageSize = 5;
+        var query = _context.BrixProducts.AsQueryable()
+            .OrderBy(x => x.Name)
+            .Skip((pageNum - 1) * pageSize)
+            .Take(pageSize);
+
+        if (!string.IsNullOrEmpty(category))
+        {
+            query = query.Where(p => p.Category == category);
+        }
+
+        if (!string.IsNullOrEmpty(primaryColor))
+        {
+            query = query.Where(p => p.PrimaryColor == primaryColor);
+        }
+
+        var products = query.OrderBy(p => p.ProductId).ToList();
+
+        ViewBag.Category = category;
+        ViewBag.PrimaryColor = primaryColor;
+        ViewBag.Categories = _context.BrixProducts.Select(p => p.Category).Distinct();
+        ViewBag.PrimaryColors = _context.BrixProducts.Select(p => p.PrimaryColor).Distinct();
+
+        return View(products);
+    }
+
+
     //
     // public IActionResult ProductDetail()
     // {
