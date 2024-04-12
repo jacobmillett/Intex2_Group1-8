45a073f8dccdcb2517308b9e1a0831115b95a0d9
reedstew <rcstew120@gmail.com>
2024-04-12 10:29:41 -0600

    final

--- obj/Debug/net8.0/AuroraBricks.AssemblyInfo.cs
+++ obj/Debug/net8.0/AuroraBricks.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AuroraBricks")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b32dabbc3df3de519d7214cf622768d358b92f37")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5a52295046f9f4446ba3d6cf24b8f12a01052596")]
 [assembly: System.Reflection.AssemblyProductAttribute("AuroraBricks")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AuroraBricks")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
